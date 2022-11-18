5e20ca53cec3ec3c4195fd661d0b2943e9b6f736
Leonardo Murça <leo@leomurca.xyz>
2022-11-18 11:21:34 -0300

    Change logo file names and smal function refactor

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,18 @@
 <script>
   import FileViewer from "./lib/FileViewer.svelte";
   import MediaQuery from "./lib/MediaQuery.svelte";
-  import logo from "./assets/logo.webp";
-  import logoMobile from "./assets/logo-mobile.webp";
+  import logo from "./assets/embroidery-viewer-logo.webp";
+  import logoMobile from "./assets/embroidery-viewer-logo-mobile.webp";
+
+  function configsFor(matches) {
+      return  matches ? { src: logoMobile, width: 350, height: 96 } : { src: logo, width: 460, height: 200 }
+  }
 </script>
 
 <header>
   <a href="/">
     <MediaQuery query="(min-width: 481px) and (max-width: 812px)" let:matches>
-      {@const configs = matches
-        ? { src: logoMobile, width: 350, height: 96 }
-        : { src: logo, width: 460, height: 200 }}
+      {@const configs = configsFor(matches)}
       <img
         class="logo"
         alt="Embroidery viewer logo."
