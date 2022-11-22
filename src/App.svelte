124ec2d80b8cb51c2a3fa15fbb4d20ca6f5824b3
Leonardo Murça <leo@leomurca.xyz>
2022-11-22 09:34:50 -0300

    Update SEO handling

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,40 @@
 <script>
+  import SvelteSeo from "svelte-seo";
   import FileViewer from "./lib/FileViewer.svelte";
   import MediaQuery from "./lib/MediaQuery.svelte";
   import logo from "./assets/embroidery-viewer-logo.webp";
   import logoMobile from "./assets/embroidery-viewer-logo-mobile.webp";
+  import thumbnail from "./assets/thumbnail.webp";
 
   function configsFor(matches) {
-      return  matches ? { src: logoMobile, width: 350, height: 96 } : { src: logo, width: 460, height: 200 }
+    return matches
+      ? { src: logoMobile, width: 350, height: 96 }
+      : { src: logo, width: 460, height: 200 };
   }
 </script>
+
+<SvelteSeo
+  title="Embroidery Viewer"
+  description="A free online tool to view embroidery files."
+  keywords="Free Emrbroidery Viewer, embroidery design, sewing machine, preview .pes files, preview embroider designs, brother machine."
+/>
+
+<SvelteSeo
+  openGraph={{
+    title: "Embroidery Viewer",
+    description: "A free online tool to view embroidery files.",
+    url: "https://embroideryviewer.xyz/",
+    type: "website",
+    images: [
+      {
+        url: thumbnail,
+        width: 1200,
+        height: 630,
+        alt: "Embroidery viewer thumbnail.",
+      },
+    ],
+  }}
+/>
 
 <header>
   <a href="/">
