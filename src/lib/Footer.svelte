d08b2956a2cfba06797d69151f336fad76434b22
Leonardo Murça <leo@leomurca.xyz>
2022-11-27 12:34:43 -0300

    Extract footer,  header and display app version

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { appVersion } from "../utils/env";
+</script>
+
+<footer>
+  <p>
+    Copyright © 2022 <a
+      href="https://leomurca.xyz"
+      target="_blank"
+      rel="noreferrer">Leonardo Murça</a
+    >.
+  </p>
+  <p>
+    version: {appVersion()}
+  </p>
+</footer>
+
+<style>
+  footer {
+    bottom: 0;
+  }
+  p {
+    text-align: center;
+  }
+</style>
