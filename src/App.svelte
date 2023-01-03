5eb454060062f76119a405ba169ac2d5668b8cfa
Leonardo Murça <leo@leomurca.xyz>
2023-01-03 16:19:45 -0300

    Extract svelte seo to a component

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,11 @@
 <script>
-  import SvelteSeo from "svelte-seo";
-  import FileViewer from "./lib/FileViewer.svelte";
-  import thumbnail from "./assets/thumbnail.webp";
-  import Footer from "./lib/Footer.svelte";
+  import Seo from "./lib/Seo.svelte";
   import Header from "./lib/Header.svelte";
-
-  const title = "Embroidery Viewer";
-  const description = "A free online tool to view embroidery files.";
-  const keywords =
-    "Free Emrbroidery Viewer, embroidery design, sewing machine, preview .pes files, preview embroider designs, brother machine.";
+  import FileViewer from "./lib/FileViewer.svelte";
+  import Footer from "./lib/Footer.svelte";
 </script>
 
-<SvelteSeo {title} {description} {keywords} />
-
-<SvelteSeo
-  openGraph={{
-    title,
-    description,
-    url: "https://embroideryviewer.xyz/",
-    type: "website",
-    images: [
-      {
-        url: thumbnail,
-        width: 1200,
-        height: 630,
-        alt: "Embroidery viewer thumbnail.",
-      },
-    ],
-  }}
-/>
-
+<Seo />
 <Header />
 <main>
   <FileViewer />
